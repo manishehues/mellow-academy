b9696aa416eaba7f15f9175365fdcf8ecdb96396
Manish Kumar <manish@ehues.com>
2022-03-09 20:24:32 +0530

    Files Deleted

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'woazuymy_academy');
+define('DB_NAME', 'mellow_academy');
 
 /** MySQL database username */
-define('DB_USER', 'woazuymy_academy');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Shinda@11');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
