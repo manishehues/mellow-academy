38403e065b04b1ea20d3d0f7333f49af4f1a5cd7
manishehues <manish@ehues.com>
2022-03-10 17:56:09 +0530

    Updated database and Css Files

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'mellow_academy');
+define('DB_NAME', 'woazuymy_academy');
 
 /** MySQL database username */
 define('DB_USER', 'root');
